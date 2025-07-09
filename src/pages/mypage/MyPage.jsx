a1720c62f03ded1d59bc1c3123c560959f1f99b3
j-hee-98 <wogml.hee@gmail.com>
2025-07-10 00:27:01 +0900

    fix : 마이페이지 ui 수정

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import ReservationList from "../../components/myPage/ReservationList";
 import ReviewList from "../../components/myPage/ReviewList";
@@ -7,24 +7,47 @@ import MeetupParticipantList from "../../components/myPage/MeetupParticipantList
 import MeetingList from "../../components/myPage/MeetingList";
 import MeetupReviewList from "../../components/myPage/MeetingReviewList";
 import "../../assets/styles/pages/myPage/myPage.css";
-// import axiosInstance from "../../api/axiosinstance";
+import axiosInstance from "../../api/axiosinstance";
+
+import mukzzangImage from "../../assets/images/먹짱.png";
+import silverImage from "../../assets/images/실버.png";
+import bronzeImage from "../../assets/images/브론즈.png";
+import sproutImage from "../../assets/images/새싹.png";
+import defaultUserImage from "../../assets/images/새싹.png";
+
+// 등급별 이미지 맵 정의
+const gradeImages = {
+  '먹짱': mukzzangImage,
+  '실버': silverImage,
+  '브론즈': bronzeImage,
+  '새싹': sproutImage,
+};
 
 const MyPage = () => {
-  const { userInfo } = useSelector((state) => state.auth);
+  // const { userInfo } = useSelector((state) => state.auth);
+  const [userForm, setUserForm] = useState([]);
+  const [userImage, setUserImage] = useState(defaultUserImage);
+
   const navigate = useNavigate();
 
   const [activeTab, setActiveTab] = useState("reservations");
   const [activeMeetingTab, setActiveMeetingTab] = useState("attended");
 
-  // useEffect(()=> {
-  //   const userInfo = async () => {
-  //     try {
-  //       const response = await axiosInstance.get("/mypage/user-info");
-  //     } catch (error) {
-  //       console.error("사용자 정보를 가져오지 못했습니다: ", error);
-  //     }
-  //   }
-  // })
+  useEffect(()=> {
+    const userInfo = async () => {
+      try {
+        const response = await axiosInstance.get("/auth/user-info");
+        setUserForm(response.data);
+
+        const imageToSet = gradeImages[userForm.userGrade] || defaultUserImage;
+        setUserImage(imageToSet);
+
+      } catch (error) {
+        console.error("사용자 정보를 가져오지 못했습니다: ", error);
+      }
+    }
+    userInfo();
+  }, []);
 
   // 현지인 인증 페이지로 이동
   const handleLocalAuth = () => {
@@ -88,14 +111,17 @@ const MyPage = () => {
   return (
     <div className="my-page-container">
       <div className="user-info-section">
-        <div className="user-id-display">{userInfo?.userId}님</div>
+        <img src={userImage} alt="사용자 등급 이미지" style={{width: '100px', height: '100px', borderRadius: '50%'}}/>
+        <div className="user-id-display">{userForm.userId}님</div>
+        <div className="user-grade">등급 : {userForm.userGrade}</div>
+        <div className="user-point">포인트 잔액 : {userForm.pointBalance}</div>
         <button
           // onClick={() => navigate("/profile-edit")}
           className="profile-edit-button"
         >
           선호도 수정
         </button>
-        <button onClick={handleLocalAuth}>현지인 인증</button>
+        <button onClick={handleLocalAuth}>동네 인증</button>
       </div>
 
       <div className="main-tabs">
