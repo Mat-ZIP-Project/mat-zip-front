90903e44182f3024be0ef03d7b0739f2230df114
chan7674 <skc3924@gmail.com>
2025-07-21 19:48:03 +0900

    chore : 웨이팅 현황 수정

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+  import { defineConfig } from "vite";
+  import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://54.180.86.163:8080', // ✅ 백엔드 서버 주소로 변경
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'), // 필요 없으면 생략 가능
+  // https://vite.dev/config/
+  export default defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://54.180.86.163:8080', // ✅ 백엔드 서버 주소로 변경
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api'), // 필요 없으면 생략 가능
+        },
       },
+      historyApiFallback: true,
     },
-    historyApiFallback: true,
-  },
-});
+  });
